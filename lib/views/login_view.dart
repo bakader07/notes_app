128f4d17a15449578f3deb1bfad964cd18a09106
bakader07 <kaderbaki07.kb@gmail.com>
2023-01-26 13:35:45 +0100

    added login invalid email exception

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -68,7 +68,9 @@ class _LoginViewState extends State<LoginView> {
                 Navigator.of(context)
                     .pushNamedAndRemoveUntil(notesRoute, (route) => false);
               } on FirebaseAuthException catch (e) {
-                if (e.code == 'user-not-found') {
+                if (e.code == 'invalid-email') {
+                  await showErrorDialog(context, 'Invalid email');
+                } else if (e.code == 'user-not-found') {
                   await showErrorDialog(context, 'User not found');
                 } else if (e.code == 'wrong-password') {
                   await showErrorDialog(context, 'Wrong password');
