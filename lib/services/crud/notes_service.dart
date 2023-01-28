1f2d803cb31e20dab2433fcb20e50cb2d02a5aa6
bakader07 <kaderbaki07.kb@gmail.com>
2023-01-28 23:35:34 +0100

    created DatabaseUser class

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -1,4 +1,20 @@
+import 'package:flutter/foundation.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart' show join;
 import 'package:path_provider/path_provider.dart'
     show getApplicationDocumentsDirectory;
+
+const idColumn = 'id';
+const emailColumn = 'email';
+
+@immutable
+class DatabaseUser {
+  final int id;
+  final String email;
+
+  DatabaseUser({required this.id, required this.email});
+
+  DatabaseUser.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        email = map[emailColumn] as String;
+}
