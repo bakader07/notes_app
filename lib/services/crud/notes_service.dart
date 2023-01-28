dec88601877245456164f4f88a9f0283911e6ecf
bakader07 <kaderbaki07.kb@gmail.com>
2023-01-28 23:42:02 +0100

    overriding toString method and equality operator

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -12,9 +12,15 @@ class DatabaseUser {
   final int id;
   final String email;
 
-  DatabaseUser({required this.id, required this.email});
+  const DatabaseUser({required this.id, required this.email});
 
   DatabaseUser.fromRow(Map<String, Object?> map)
       : id = map[idColumn] as int,
         email = map[emailColumn] as String;
+
+  @override
+  String toString() => 'User, ID: $id, Email: $email';
+
+  @override
+  bool operator ==(covariant DatabaseUser other) => id == other.id;
 }
