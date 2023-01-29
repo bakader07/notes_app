76bd4522364afca694f3f5a91b038ff519106b4d
bakader07 <kaderbaki07.kb@gmail.com>
2023-01-29 17:28:22 +0100

    fixed auth test required email field

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -130,7 +130,7 @@ class MockAuthProvider implements AuthProvider {
     if (email == 'test@mail.com') throw UserNotFoundAuthException();
     if (password == 'password') throw WrongPasswordAuthException();
 
-    const user = AuthUser(isEmailVerified: false);
+    const user = AuthUser(isEmailVerified: false, email: 'foo@bar.com');
     _user = user;
     return Future.value(user);
   }
@@ -150,7 +150,7 @@ class MockAuthProvider implements AuthProvider {
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
 
-    const newUser = AuthUser(isEmailVerified: true);
+    const newUser = AuthUser(isEmailVerified: true, email: 'foo@bar.com');
     _user = newUser;
   }
 }
