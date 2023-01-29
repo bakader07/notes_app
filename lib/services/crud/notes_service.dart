6ab046d77cc551a52cb905ec7b931d43c76f0c32
bakader07 <kaderbaki07.kb@gmail.com>
2023-01-29 01:37:19 +0100

    created NotesService class with open close methods

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -2,7 +2,44 @@ import 'package:flutter/foundation.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart' show join;
 import 'package:path_provider/path_provider.dart'
-    show getApplicationDocumentsDirectory;
+    show MissingPlatformDirectoryException, getApplicationDocumentsDirectory;
+
+class UnableToGetDocumentDirectoryException implements Exception {}
+
+class DatabaseAlreadyOpenException implements Exception {}
+
+class DatabaseIsNotOpenException implements Exception {}
+
+class NotesService {
+  Database? _db;
+
+  Future<void> open() async {
+    if (_db != null) {
+      throw DatabaseAlreadyOpenException();
+    }
+    try {
+      final docsPath = await getApplicationDocumentsDirectory();
+      final dbPath = join(docsPath.path, dbName);
+      final db = await openDatabase(dbPath);
+      _db = db;
+      // Create users table
+      await db.execute(createUsersTable);
+      // Create notes table
+      await db.execute(createNotesTable);
+    } on MissingPlatformDirectoryException {
+      throw UnableToGetDocumentDirectoryException();
+    }
+  }
+
+  Future<void> close() async {
+    final db = _db;
+    if (db == null) {
+      throw DatabaseIsNotOpenException();
+    }
+    await db.close();
+    _db = null;
+  }
+}
 
 @immutable
 class DatabaseUser {
@@ -56,8 +93,27 @@ class DatabaseNote {
   int get hashCode => id.hashCode;
 }
 
+const dbName = 'notes.db';
+const usersTable = 'users';
+const notesTable = 'notes';
+
 const idColumn = 'id';
 const emailColumn = 'email';
 const userIdColumn = 'user_id';
 const textColumn = 'text';
 const isSycedWithCloudColumn = 'is_syced_with_cloud';
+
+const createUsersTable = '''CREATE TABLE IF NOT EXISTS "$usersTable" (
+  "$idColumn" INTEGER NOT NULL,
+  "$emailColumn" TEXT NOT NULL UNIQUE,
+  PRIMARY KEY ("$idColumn", AUTOINCREMENT)
+);''';
+
+const createNotesTable = '''CREATE TABLE IF NOT EXISTS "$notesTable" (
+  "$idColumn" INTEGER NOT NULL,
+  "$userIdColumn" INTEGER NOT NULL UNIQUE,
+  "$textColumn" TEXT,
+  "$isSycedWithCloudColumn" INTEGER NOT NULL UNIQUE DEFAULT 0,
+  FOREIGN KEY ("$userIdColumn") REFRENCES "$usersTable"("$idColumn"),
+  PRIMARY KEY ("$idColumn", AUTOINCREMENT)
+);''';
