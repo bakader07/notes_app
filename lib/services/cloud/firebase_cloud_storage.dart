153a308ecbb9775214bac9b4de6b6465b8a7afd0
bakader07 <kaderbaki07.kb@gmail.com>
2023-02-02 14:09:13 +0100

    added a singleton FirebaseCloudStorage

---
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -0,0 +1,9 @@
+class FirebaseCloudStorage {
+  // static instance
+  static final _storage = FirebaseCloudStorage._instance();
+
+  factory FirebaseCloudStorage() => _storage;
+
+  // Constructor
+  FirebaseCloudStorage._instance();
+}
